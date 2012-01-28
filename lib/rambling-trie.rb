914813e67d31c2b687f35272791d6bdf85fc4da3
Edgar Gonzalez <edgar@ramblinglabs.com>
2012-01-28 17:07:49 -0400

    Added the as_word method with first tests.

--- lib/rambling-trie.rb
+++ lib/rambling-trie.rb
@@ -1,3 +1,4 @@
+require File.join(File.dirname(__FILE__), 'invalid_trie_operation.rb')
 require File.join(File.dirname(__FILE__), 'trie_node.rb')
 require File.join(File.dirname(__FILE__), 'trie.rb')
 
