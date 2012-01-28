2260c1c2ad675983e7a4ca239f4332cf5f6d0334
Edgar Gonzalez <edgar@ramblinglabs.com>
2012-01-27 22:54:44 -0400

    Added the 'Rambling' module for namespacing.

--- spec/lib/trie_node_spec.rb
+++ spec/lib/trie_node_spec.rb
@@ -1,79 +1,81 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
-describe TrieNode do
-  describe 'when creating a node with one letter' do
-    trie_node = nil
+module Rambling
+  describe TrieNode do
+    describe 'when creating a node with one letter' do
+      trie_node = nil
 
-    before(:each) do
-      trie_node = TrieNode.new('a')
+      before(:each) do
+        trie_node = TrieNode.new('a')
+      end
+
+      it 'should make it the node letter' do
+        trie_node.letter.should == 'a'
+      end
+
+      it 'should include no children' do
+        trie_node.children.should be_empty
+      end
+
+      it 'should be a terminal node' do
+        trie_node.terminal?.should be_true
+      end
     end
 
-    it 'should make it the node letter' do
-      trie_node.letter.should == 'a'
+    describe 'when creating a node with two letters' do
+      trie_node = nil
+
+      before(:each) do
+        trie_node = TrieNode.new('ba')
+      end
+
+      it 'should take the first as the node letter' do
+        trie_node.letter.should == 'b'
+      end
+
+      it 'should include one child' do
+        trie_node.children.should_not be_empty
+        trie_node.children.length.should == 1
+      end
+
+      it 'should include a child with the expected letter' do
+        trie_node.children.values.first.letter.should == 'a'
+      end
     end
 
-    it 'should include no children' do
-      trie_node.children.should be_empty
+    describe 'when adding a child that already exists' do
+      trie_node = nil
+
+      before(:each) do
+        trie_node = TrieNode.new('ba')
+        trie_node.add_branch_from('a')
+      end
+
+      it 'should not increment the child count' do
+        trie_node.children.length.should == 1
+      end
     end
 
-    it 'should be a terminal node' do
-      trie_node.terminal?.should be_true
-    end
-  end
+    describe 'when finding out if a child exists' do
+      trie_node = nil
 
-  describe 'when creating a node with two letters' do
-    trie_node = nil
+      before(:each) do
+        trie_node = TrieNode.new('back')
+        trie_node.add_branch_from('ull')
+      end
 
-    before(:each) do
-      trie_node = TrieNode.new('ba')
-    end
+      it 'should return true for an existing child' do
+        trie_node.has_branch_tree?('a').should be_true
+        trie_node.has_branch_tree?('ack').should be_true
+        trie_node.has_branch_tree?('u').should be_true
+        trie_node.has_branch_tree?('ull').should be_true
+      end
 
-    it 'should take the first as the node letter' do
-      trie_node.letter.should == 'b'
-    end
-
-    it 'should include one child' do
-      trie_node.children.should_not be_empty
-      trie_node.children.length.should == 1
-    end
-
-    it 'should include a child with the expected letter' do
-      trie_node.children.values.first.letter.should == 'a'
-    end
-  end
-
-  describe 'when adding a child that already exists' do
-    trie_node = nil
-
-    before(:each) do
-      trie_node = TrieNode.new('ba')
-      trie_node.add_branch_from('a')
-    end
-
-    it 'should not increment the child count' do
-      trie_node.children.length.should == 1
-    end
-  end
-
-  describe 'when finding out if a child exists' do
-    trie_node = nil
-
-    before(:each) do
-      trie_node = TrieNode.new('back')
-      trie_node.add_branch_from('ull')
-    end
-
-    it 'should return true for an existing child' do
-      trie_node.has_branch_tree?('a').should be_true
-      trie_node.has_branch_tree?('ack').should be_true
-      trie_node.has_branch_tree?('u').should be_true
-      trie_node.has_branch_tree?('ull').should be_true
-    end
-
-    it 'should return false for a non existing child' do
-      trie_node.has_branch_tree?('b').should be_false
-      trie_node.has_branch_tree?('x').should be_false
-      trie_node.has_branch_tree?('ll').should be_false
+      it 'should return false for a non existing child' do
+        trie_node.has_branch_tree?('b').should be_false
+        trie_node.has_branch_tree?('x').should be_false
+        trie_node.has_branch_tree?('ll').should be_false
+      end
     end
   end
 end
--- spec/lib/trie_spec.rb
+++ spec/lib/trie_spec.rb
@@ -1,45 +1,47 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
-describe Trie do
-  describe 'when initializing a trie without filename' do
-    trie = nil
+module Rambling
+  describe Trie do
+    describe 'when initializing a trie without filename' do
+      trie = nil
 
-    before(:each) do
-      trie = Trie.new
-    end
+      before(:each) do
+        trie = Trie.new
+      end
 
-    it 'should have no letter' do
-      trie.letter.should be_nil
-    end
+      it 'should have no letter' do
+        trie.letter.should be_nil
+      end
 
-    it 'should not be a terminal node' do
-      trie.terminal?.should be_false
-    end
+      it 'should not be a terminal node' do
+        trie.terminal?.should be_false
+      end
 
-    it 'should have no children' do
-      trie.children.should be_empty
-    end
-  end
-
-  describe 'when initializing a trie from a file' do
-    trie = nil
-    filename = File.join(File.dirname(__FILE__), '..', 'assets', 'test_words.txt')
-
-    before(:each) do
-      trie = Trie.new(filename)
-    end
-
-    it 'should add the expected root children' do
-      File.open(filename) do |file|
-        expected_hash = Hash[file.readlines.map { |x| [x.slice(0), nil] }]
-        trie.children.length.should == expected_hash.length
-        trie.children.map { |k, v| k }.should =~ expected_hash.map { |k, v| k }
+      it 'should have no children' do
+        trie.children.should be_empty
       end
     end
 
-    it 'should load all words' do
-      File.open(filename) do |file|
-        file.readlines.each { |word| trie.has_branch_tree?(word.chomp).should be_true }
+    describe 'when initializing a trie from a file' do
+      trie = nil
+      filename = File.join(File.dirname(__FILE__), '..', 'assets', 'test_words.txt')
+
+      before(:each) do
+        trie = Trie.new(filename)
+      end
+
+      it 'should add the expected root children' do
+        File.open(filename) do |file|
+          expected_hash = Hash[file.readlines.map { |x| [x.slice(0), nil] }]
+          trie.children.length.should == expected_hash.length
+          trie.children.map { |k, v| k }.should =~ expected_hash.map { |k, v| k }
+        end
+      end
+
+      it 'should load all words' do
+        File.open(filename) do |file|
+          file.readlines.each { |word| trie.has_branch_tree?(word.chomp).should be_true }
+        end
       end
     end
   end
