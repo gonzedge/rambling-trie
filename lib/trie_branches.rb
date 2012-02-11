8acd8d2a7b0dc22290cc0f659507b0c21071d8d9
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-02-11 02:13:47 -0400

    Fixed `has_branch_for?` for compressed trie.

--- lib/trie_branches.rb
+++ lib/trie_branches.rb
@@ -1,11 +1,30 @@
 module Rambling
   module TrieBranches
     def has_branch_for?(word)
-      word.empty? or branch_exists_and(word, :has_branch_for?)
+      return true if word.empty?
+
+      keys = @children.keys.map { |x| x.to_s }
+      return true if keys.include?(word)
+
+      partial_key = keys.select { |x| x.start_with?(word) }.first
+      return true unless partial_key.nil?
+
+      key = keys.select { |x| word.start_with?(x) }.first
+      return @children[key.to_sym].has_branch_for?(word.slice(key.length..word.length)) unless key.nil?
+
+      false
     end
 
     def is_word?(word = '')
-      (word.empty? and terminal?) or branch_exists_and(word, :is_word?)
+      return true if word.empty? and terminal?
+
+      length = word.length
+      for index in (0...length)
+        key = word.slice(0..index).to_sym
+        return @children[key].is_word?(word.slice((index + 1)...length)) if @children.has_key?(key)
+      end
+
+      false
     end
 
     def add_branch_from(word)
@@ -23,20 +42,6 @@ module Rambling
       else
         @children[first_letter] = TrieNode.new(word, self)
       end
-    end
-
-    private
-
-    def branch_exists_and(word, method)
-      unless word.empty?
-        length = word.length
-        for index in (0..length)
-          key = word.slice(0..index).to_sym
-          return @children[key].send(method, word.slice((index + 1)..length)) if @children.has_key?(key)
-        end
-      end
-
-      false
     end
   end
 end
