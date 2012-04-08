d51d40bf395d32088493260e0c4c4465de4e3f66
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-04-08 16:39:15 -0400

    Refactored rambling-trie requires.

--- lib/rambling-trie.rb
+++ lib/rambling-trie.rb
@@ -1,8 +1,12 @@
-require File.join(File.dirname(__FILE__), 'rambling.rb')
-require File.join(File.dirname(__FILE__), 'invalid_trie_operation.rb')
-require File.join(File.dirname(__FILE__), 'children_hash_deferer.rb')
-require File.join(File.dirname(__FILE__), 'trie_compressor.rb')
-require File.join(File.dirname(__FILE__), 'trie_branches.rb')
-require File.join(File.dirname(__FILE__), 'trie_node.rb')
-require File.join(File.dirname(__FILE__), 'trie.rb')
-require File.join(File.dirname(__FILE__), 'rambling-trie', 'version.rb')
+[
+  'rambling',
+  'invalid_trie_operation',
+  'children_hash_deferer',
+  'trie_compressor',
+  'trie_branches',
+  'trie_node',
+  'trie',
+  'rambling-trie/version'
+].each do |file|
+  require File.join File.dirname(__FILE__), file
+end
