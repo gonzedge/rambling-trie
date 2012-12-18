c44f3f635e9f1943d61826d46b7c241265e69067
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-12-18 04:26:30 -0400

    Added #inspect documentation.

--- lib/rambling/trie/inspector.rb
+++ lib/rambling/trie/inspector.rb
@@ -2,6 +2,7 @@ module Rambling
   module Trie
     # Provides pretty printing behavior for the Trie data structure.
     module Inspector
+      # @return [String] a string representation of the current node.
       def inspect
         "#<#{self.class.name} letter: #{letter.inspect or 'nil'}, children: #{children.keys}>"
       end
