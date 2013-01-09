44269350f290fcdbee145d22ddac603457d80836
Edgar Gonzalez <edggonzalezg@gmail.com>
2013-01-09 02:19:15 -0400

    Default rspec output to documentation and syntax to 'expect'.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,15 @@ SimpleCov.start do
   add_filter '/spec/'
 end
 
+require 'rspec'
 require 'rambling-trie'
 ::SPEC_ROOT = File.dirname(__FILE__)
 
 RSpec.configure do |config|
   config.order = :random
+  config.formatter = :documentation
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
