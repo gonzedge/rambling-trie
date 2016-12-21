fc2e1d8d252b436436760a8dc41c9403b239fa7f
Edgar Gonzalez <edggonzalezg@gmail.com>
2016-12-21 00:33:24 -0500

    Refactor Compressor methods

--- lib/rambling/trie/compressor.rb
+++ lib/rambling/trie/compressor.rb
@@ -2,39 +2,42 @@ module Rambling
   module Trie
     # Responsible for the compression process of a Trie data structure.
     class Compressor
-      # Compresses a Trie data structure.
-      # @param [RawNode] root the root of the Trie data structure
-      # @return [CompressedNode] root the root of the Trie data structure
-      def compress root
-        new_root = Rambling::Trie::CompressedNode.new
-
-        root.children.each do |child|
-          compress_child child, new_root
+      # Compresses a node from a Trie data structure.
+      # @param [RawNode] node the node to compress
+      # @return [CompressedNode] node the compressed version of the node
+      def compress node, parent = nil
+        if node.compressable?
+          merge_node_with_compressed_child node, parent
+        else
+          copy_node_and_compress_children node, parent
         end
-
-        new_root
       end
 
       private
 
-      def compress_child node, parent = nil
+      def merge_node_with_compressed_child node, parent
+        compressed_child = compress node.children.first
+
+        new_node = Rambling::Trie::CompressedNode.new parent
+        new_node.letter = node.letter.to_s << compressed_child.letter.to_s
+        new_node.terminal = compressed_child.terminal?
+        new_node.children_tree = compressed_child.children_tree
+
+        new_node.children.each do |child|
+          child.parent = new_node
+        end
+
+        new_node
+      end
+
+      def copy_node_and_compress_children node, parent
         new_node = Rambling::Trie::CompressedNode.new parent
 
-        if node.compressable?
-          merged_node = compress_child node.children.first
+        new_node.letter = node.letter
+        new_node.terminal = node.terminal?
 
-          new_node.letter = node.letter.to_s << merged_node.letter.to_s
-          new_node.terminal = merged_node.terminal?
-
-          merged_node.children.each { |c| c.parent = new_node }
-          new_node.children_tree = merged_node.children_tree
-        else
-          new_node.letter = node.letter
-          new_node.terminal = node.terminal?
-
-          node.children.map do |child|
-            compress_child child, new_node
-          end
+        node.children.map do |child|
+          compress child, new_node
         end
 
         new_node
