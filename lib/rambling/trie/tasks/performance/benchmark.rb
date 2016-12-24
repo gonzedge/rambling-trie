5b831fc4ec30451bef9ab183d362b6a1c75a081f
Edgar Gonzalez <edggonzalezg@gmail.com>
2016-12-23 23:46:26 -0500

    Add more benchmark tasks

--- lib/rambling/trie/tasks/performance/benchmark.rb
+++ lib/rambling/trie/tasks/performance/benchmark.rb
@@ -1,57 +1,120 @@
+require_relative '../helpers/path'
 
 namespace :performance do
   include Helpers::Path
 
-  def benchmark name, trie, output
-    words = %w(hi help beautiful impressionism anthropological)
-    methods = [:word?, :partial_word?]
+  def measure times, param, output
+    measure = Benchmark.measure do
+      times.times do
+        yield param
+      end
+    end
+    output.puts measure
+  end
 
-    output.puts "==> #{name}"
-    methods.each do |method|
-      output.puts "`#{method}`:"
-      words.each do |word|
-        output.print "#{word} - #{trie.send method, word}".ljust 30
-        output.puts Benchmark.measure { 200_000.times { trie.send method, word }}
+  def perform_benchmark name, times, params, output
+    output.puts "`#{name}`:"
+
+    params = Array params
+    if params.any?
+      params.each do |param|
+        output.print "#{param}".ljust 30
+
+        measure times, param, output do |param|
+          yield param
+        end
+      end
+    else
+      measure times, nil, output do |param|
+        yield param
       end
     end
   end
 
-  def generate_benchmark filename = nil
-    output = filename.nil? ? $stdout : File.open(filename, 'a+')
+  def benchmark_lookups name, trie, output
+    words = %w(hi help beautiful impressionism anthropological)
 
-    output.puts "\nBenchmark for rambling-trie version #{Rambling::Trie::VERSION}"
-
-    trie = nil
-    measure = Benchmark.measure { trie = Rambling::Trie.create path('assets', 'dictionaries', 'words_with_friends.txt') }
-
-    if ENV['profile_creation']
-      output.puts '==> Creation'
-      output.print 'Rambling::Trie.create'.ljust 30
-      output.puts measure
+    output.puts "==> #{name}"
+    perform_benchmark 'word?', 200_000, words, output do |word|
+      trie.word? word
     end
 
-    benchmark 'Uncompressed', trie, output
+    perform_benchmark 'partial_word?', 200_000, words, output do |word|
+      trie.partial_word? word
+    end
+  end
 
-    return unless trie.respond_to? :compress!
+  def with_file filename = nil
+    output = filename.nil? ? IO.new(1) : File.open(filename, 'a+')
 
-    trie.compress!
-    benchmark 'Compressed', trie, output
+    yield output
 
     output.close
   end
 
-  desc 'Generate performance benchmark report'
-  task :benchmark do
-    puts 'Generating performance benchmark report...'
-    generate_benchmark
+  def generate_lookups_benchmark filename = nil
+    with_file filename do |output|
+      output.puts "\nBenchmark for rambling-trie version #{Rambling::Trie::VERSION}"
+
+      trie = Rambling::Trie.create path('assets', 'dictionaries', 'words_with_friends.txt')
+      benchmark_lookups 'Uncompressed', trie, output
+
+      trie.compress!
+      benchmark_lookups 'Compressed', trie, output
+    end
   end
 
   namespace :benchmark do
-    desc 'Generate performance benchmark report store results in reports/'
-    task save: ['performance:directory'] do
+    desc 'Generate performance benchmark report'
+    task :lookups do
       puts 'Generating performance benchmark report...'
-      generate_benchmark path('reports', Rambling::Trie::VERSION, 'benchmark')
-      puts "Benchmarks have been saved to reports/#{Rambling::Trie::VERSION}/benchmark"
+      generate_lookups_benchmark
+    end
+
+    namespace :lookups do
+      desc 'Generate performance benchmark report store results in reports/'
+      task save: ['performance:directory'] do
+        puts 'Generating performance benchmark report...'
+        generate_lookups_benchmark path('reports', Rambling::Trie::VERSION, 'benchmark')
+        puts "Benchmarks have been saved to reports/#{Rambling::Trie::VERSION}/benchmark-lookups"
+      end
+    end
+
+    task :creation do
+      with_file do |output|
+        output.puts "\nBenchmark for rambling-trie version #{Rambling::Trie::VERSION}"
+        output.puts '==> Creation'
+
+        perform_benchmark '`Rambling::Trie.create`', 5, [], output do
+          trie = Rambling::Trie.create path('assets', 'dictionaries', 'words_with_friends.txt')
+        end
+      end
+    end
+
+    task :compression do
+      with_file do |output|
+        output.puts "\nBenchmark for rambling-trie version #{Rambling::Trie::VERSION}"
+        output.puts '==> Compression'
+
+        trie = Rambling::Trie.create path('assets', 'dictionaries', 'words_with_friends.txt')
+        perform_benchmark '`compress!`', 5, nil, output do
+          trie.clone.compress!
+        end
+      end
+    end
+
+    task :compare do
+      Benchmark.ips do |b|
+        hash = { 'thing' => 'gniht' }
+
+        b.report 'has_key?' do
+          hash.has_key? 'thing'
+        end
+
+        b.report '[]' do
+          !!hash['thing']
+        end
+      end
     end
   end
 end
