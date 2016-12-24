b0590401856a6a31311b47eca136d7bd98af3d67
Edgar Gonzalez <edggonzalezg@gmail.com>
2016-12-24 18:10:24 -0500

    Refactor call_tree profiling, use different filepath structure

--- lib/rambling/trie/tasks/performance/profile/call_tree.rb
+++ lib/rambling/trie/tasks/performance/profile/call_tree.rb
@@ -2,34 +2,55 @@ require_relative '../../helpers/path'
 
 namespace :performance do
   namespace :profile do
-    desc 'Generate application profiling reports'
-    task :call_tree do
-      puts 'Generating call tree profiling reports...'
+    def profile times, params, path
+      result = RubyProf.profile merge_fibers: true do
+        params.each do |param|
+          times.times do
+            yield param
+          end
+        end
+      end
 
+      printer = RubyProf::CallTreePrinter.new result
+      printer.print path: path
+    end
+
+    def generate_lookups_call_tree
+      puts 'Generating call tree profiling reports for lookups...'
+
+      puts "\nCall Tree profile for rambling-trie version #{Rambling::Trie::VERSION}"
       trie = Rambling::Trie.create path('assets', 'dictionaries', 'words_with_friends.txt')
+      tries = [ trie, trie.clone.compress! ]
+
       words = %w(hi help beautiful impressionism anthropological)
-      methods = [:word?, :partial_word?]
-      tries = [lambda {trie.clone}, lambda {trie.clone.compress!}]
       time = Time.now.to_i
 
-      methods.each do |method|
-        tries.each do |trie_generator|
-          trie = trie_generator.call
-          result = RubyProf.profile merge_fibers: true do
-            words.each do |word|
-              200_000.times { trie.send method, word }
-            end
-          end
+      tries.each do |trie|
+        filename = "profile-#{trie.compressed? ? 'compressed' : 'uncompressed'}-word"
+        path = path 'reports', Rambling::Trie::VERSION, 'call-tree', time.to_s, filename
+        FileUtils.mkdir_p path
 
-          path = path('reports', Rambling::Trie::VERSION, "#{time}-profile-#{trie.compressed? ? 'compressed' : 'uncompressed'}-#{method.to_s.sub(/\?/, '')}")
+        profile 200_000, words, path do
+          trie.word? word
+        end
 
-          FileUtils.mkdir_p path
-          printer = RubyProf::CallTreePrinter.new result
-          printer.print path: path
+        filename = "profile-#{trie.compressed? ? 'compressed' : 'uncompressed'}-partial-word"
+        path = path 'reports', Rambling::Trie::VERSION, 'call-tree', time.to_s, filename
+        FileUtils.mkdir_p path
+
+        profile 200_000, words, path do
+          trie.partial_word? word
         end
       end
 
       puts 'Done'
+    end
+
+    namespace :call_tree do
+      desc 'Generate application profiling reports for lookups'
+      task :lookups do
+        generate_lookups_call_tree
+      end
     end
   end
 end
