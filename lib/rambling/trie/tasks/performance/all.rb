75e02be9fd5c52b7580b9e4b588eb44f9fd5f0cd
Edgar Gonzalez <edggonzalezg@gmail.com>
2016-12-26 16:41:14 -0500

    Add performance:all:save task

--- lib/rambling/trie/tasks/performance/all.rb
+++ lib/rambling/trie/tasks/performance/all.rb
@@ -1,8 +1,17 @@
 namespace :performance do
-  desc 'Generate profiling and performance reports'
+  desc 'Generate all profiling and performance reports'
   task all: [
     'benchmark:all',
     'profile:call_tree:all',
     'profile:memory:all',
   ]
+
+  namespace :all do
+    desc 'Generate and store all profiling and performance reports'
+    task save: [
+      'benchmark:all:save',
+      'profile:call_tree:all',
+      'profile:memory:all',
+    ]
+  end
 end
