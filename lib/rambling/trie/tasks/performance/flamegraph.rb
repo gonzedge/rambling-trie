f96069c122be159d988c3752f7177de22957c55b
Edgar Gonzalez <edggonzalezg@gmail.com>
2017-01-06 01:09:58 -0500

    Bring flamegraph task closer to benchmark, call_tree and memory

--- lib/rambling/trie/tasks/performance/flamegraph.rb
+++ lib/rambling/trie/tasks/performance/flamegraph.rb
@@ -2,53 +2,75 @@ namespace :performance do
   include Helpers::Path
   include Helpers::Time
 
-  def generate times, params, path
-    params = Array params
-    params << nil unless params.any?
+  def performance_report
+    @performance_report ||= PerformanceReport.new
+  end
 
-    result = Flamegraph.generate path do
-      params.each do |param|
-        times.times do
-          yield param
+  def output
+    performance_report.output
+  end
+
+  class FlamegraphProfile
+    def initialize filename
+      @filename = filename
+    end
+
+    def perform times, params = nil
+      params = Array params
+      params << nil unless params.any?
+
+      dirname = path 'reports', Rambling::Trie::VERSION, 'flamegraph', time
+      FileUtils.mkdir_p dirname
+      path = File.join dirname, "#{filename}.html"
+
+      result = Flamegraph.generate path do
+        params.each do |param|
+          times.times do
+            yield param
+          end
         end
       end
     end
+
+    private
+
+    attr_reader :filename
   end
 
   namespace :flamegraph do
+    desc 'Output banner'
+    task :banner do
+      performance_report.start 'Flamegraph'
+    end
+
     desc 'Generate flamegraph reports for creation'
-    task creation: ['performance:directory'] do
-      puts 'Generating flamegraph reports for creation...'
-      puts "\nFlamegraph for rambling-trie version #{Rambling::Trie::VERSION}"
-      directory = path 'reports', Rambling::Trie::VERSION, 'flamegraph', time
-      FileUtils.mkdir_p directory
+    task creation: ['performance:directory', :banner] do
+      output.puts 'Generating flamegraph reports for creation...'
 
-      filename = 'new-trie.html'
-      path = File.join directory, filename
-
-      generate 1, nil, path do
+      flamegraph = FlamegraphProfile.new 'new-trie'
+      flamegraph.perform 1 do
         trie = Rambling::Trie.create dictionary
       end
     end
 
     desc 'Generate flamegraph reports for compression'
-    task compression: ['performance:directory'] do
-      puts 'Generating flamegraph reports for compression...'
-      puts "\nFlamegraph for rambling-trie version #{Rambling::Trie::VERSION}"
-
-      directory = path 'reports', Rambling::Trie::VERSION, 'flamegraph', time
-      FileUtils.mkdir_p directory
-
-      filename = 'compressed-trie.html'
-      path = File.join directory, filename
+    task compression: ['performance:directory', :banner] do
+      output.puts 'Generating flamegraph reports for compression...'
 
       tries = []
       1.times { tries << Rambling::Trie.create(dictionary) }
 
-      generate 1, tries, path do |trie|
+      flamegraph = FlamegraphProfile.new 'compressed-trie'
+      flamegraph.perform 1, tries do |trie|
         trie.compress!
         nil
       end
     end
+
+    desc 'Generate all flamegraph reports'
+    task all: [
+      :creation,
+      :compression,
+    ]
   end
 end
