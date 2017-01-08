769a9e9f3562212926742b9cff0c7a4b2d43e9e0
Edgar Gonzalez <edggonzalezg@gmail.com>
2017-01-07 21:22:50 -0500

    Ensure time is the same for all performance tasks that use it

--- lib/rambling/trie/tasks/helpers/time.rb
+++ lib/rambling/trie/tasks/helpers/time.rb
@@ -1,7 +1,7 @@
 module Helpers
   module Time
     def time
-      @time ||= ::Time.now.strftime '%Y-%m-%d %H.%M.%S.%L'
+      @@time ||= ::Time.now.strftime '%Y-%m-%d %H.%M.%S.%L'
     end
   end
 end
