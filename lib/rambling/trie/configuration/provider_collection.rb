ae5e108a204374eb0346e303128f487d96287f35
Edgar Gonzalez <edggonzalezg@gmail.com>
2017-01-09 00:12:19 -0500

    Add documentation for Configuration and its classes
    - Add docs for Properties
    - Add docs for ProviderCollection
    
    [Related to #11]

--- lib/rambling/trie/configuration/provider_collection.rb
+++ lib/rambling/trie/configuration/provider_collection.rb
@@ -1,10 +1,27 @@
 module Rambling
   module Trie
     module Configuration
+      # Collection of configurable providers.
       class ProviderCollection
         extend Rambling::Trie::Forwardable
 
-        attr_reader :default, :name
+        # The name of this provider collection.
+        # @return [String] the name of this provider collection.
+        attr_reader :name
+
+        # @overload default
+        #   The default provider. Used when a provider cannot be resolved in
+        #   {ProviderCollection#resolve #resolve}.
+        # @overload default=(provider)
+        #   Sets the default provider. Needs to be one of the configured
+        #   providers.
+        #   @param [Object] provider the provider to use as default.
+        #   @raise [ArgumentError] when the given provider is not in the
+        #     provider collection.
+        #   @note If no providers have been configured, `nil` will be assigned.
+        # @return [Object, nil] the default provider to use when a provider
+        #   cannot be resolved in {ProviderCollection#resolve #resolve}.
+        attr_reader :default
 
         delegate [
           :[],
@@ -13,6 +30,10 @@ module Rambling
           :values,
         ] => :providers
 
+        # Creates a new provider collection.
+        # @param [String] name the name for this provider collection.
+        # @param [Hash] providers the configured providers.
+        # @param [Object] default the configured default provider.
         def initialize name, providers = {}, default = nil
           @name = name
           @configured_providers = providers
@@ -21,6 +42,11 @@ module Rambling
           reset
         end
 
+        # Adds a new provider to the provider collection.
+        # @param [Symbol] extension the extension that the provider will
+        #   correspond to.
+        # @param [provider] provider the provider to add to the provider
+        #   collection.
         def add extension, provider
           providers[extension] = provider
         end
@@ -33,14 +59,22 @@ module Rambling
           @default = provider
         end
 
+        # List of configured providers.
+        # @return [Hash] the mapping of extensions to their corresponding
+        #   providers.
         def providers
           @providers ||= {}
         end
 
+        # Resolves the provider from a filepath based on the file extension.
+        # @param [String] filepath the filepath to resolve into a provider.
+        # @return [Object] the provider corresponding to the file extension in
+        #   this provider collection. {#default} if not found.
         def resolve filepath
           providers[format filepath] || default
         end
 
+        # Resets the provider collection to the initial values.
         def reset
           providers.clear
           configured_providers.each { |k, v| providers[k] = v }
