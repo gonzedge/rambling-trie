12d06c59807f22aa93dc1ddd015ea38a02f0114e
Edgar Gonzalez <edggonzalezg@gmail.com>
2017-01-08 23:43:53 -0500

    Leave default as nil when no providers are in provider_collection
    [Related to #11]

--- spec/lib/rambling/trie/configuration/provider_collection_spec.rb
+++ spec/lib/rambling/trie/configuration/provider_collection_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
 describe Rambling::Trie::Configuration::ProviderCollection do
+  let(:configured_default) { nil }
+  let(:configured_providers) do
+    { one: first_provider, two: second_provider }
+  end
+
   let(:first_provider) { double :first_provider }
   let(:second_provider) { double :second_provider }
-  let(:default) { nil }
 
   let(:provider_collection) do
     Rambling::Trie::Configuration::ProviderCollection.new(
       'provider',
-      { one: first_provider, two: second_provider },
-      default
+      configured_providers,
+      configured_default
     )
   end
 
@@ -28,7 +32,7 @@ describe Rambling::Trie::Configuration::ProviderCollection do
     end
 
     context 'when a default is provided' do
-      let(:default) { second_provider }
+      let(:configured_default) { second_provider }
 
       it 'has that as the default provider' do
         expect(provider_collection.default).to eq second_provider
@@ -82,6 +86,8 @@ describe Rambling::Trie::Configuration::ProviderCollection do
   end
 
   describe '#default=' do
+    let(:other_provider) { double :other_provider }
+
     context 'when the given value is in the providers list' do
       it 'changes the default provider' do
         provider_collection.default = second_provider
@@ -90,12 +96,10 @@ describe Rambling::Trie::Configuration::ProviderCollection do
     end
 
     context 'when the given value is not in the providers list' do
-      let(:provider) { double :provider }
-
       it 'does not change the default provider' do
         expect do
           begin
-            provider_collection.default = provider
+            provider_collection.default = other_provider
           rescue
           end
         end.not_to change { provider_collection.default }
@@ -103,8 +107,26 @@ describe Rambling::Trie::Configuration::ProviderCollection do
 
       it 'raises an ArgumentError' do
         expect do
-          provider_collection.default = provider
+          provider_collection.default = other_provider
         end.to raise_error ArgumentError
+      end
+    end
+
+    context 'when the providers list is empty' do
+      let(:configured_providers) do
+        {}
+      end
+
+      it 'accepts nil' do
+        provider_collection.default = nil
+        expect(provider_collection.default).to be_nil
+      end
+
+      it 'raises an ArgumentError for any other provider' do
+        expect do
+          provider_collection.default = other_provider
+        end.to raise_error ArgumentError
+        expect(provider_collection.default).to be_nil
       end
     end
   end
@@ -126,7 +148,7 @@ describe Rambling::Trie::Configuration::ProviderCollection do
   end
 
   describe '#reset' do
-    let(:default) { second_provider }
+    let(:configured_default) { second_provider }
     let(:provider) { double :provider }
 
     before do
