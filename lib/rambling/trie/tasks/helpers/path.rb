0a42e4976c37d2368755ba9dbec1f728f8406ec2
Edgar Gonzalez <edggonzalezg@gmail.com>
2017-01-09 23:15:46 -0500

    Add serialization benchmarks
    [Related to #10]

--- lib/rambling/trie/tasks/helpers/path.rb
+++ lib/rambling/trie/tasks/helpers/path.rb
@@ -8,6 +8,14 @@ module Helpers
       path 'assets', 'dictionaries', 'words_with_friends.txt'
     end
 
+    def raw_trie_path
+      path 'assets', 'tries', 'words_with_friends_raw.marshal'
+    end
+
+    def compressed_trie_path
+      path 'assets', 'tries', 'words_with_friends_compressed.marshal'
+    end
+
     private
 
     def full_path *filename
