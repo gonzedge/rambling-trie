459c8afa3bdf0eb1b921fa93de0ff7d4ed7b936a
Edgar Gonzalez <edggonzalezg@gmail.com>
2023-05-11 13:33:05 -0400

    Add Readers::Reader and Serializer::Serializer base classes
    For clearer documentation and guidelines

---
+++ lib/rambling/trie/readers/reader.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Rambling
+  module Trie
+    module Readers
+      # Base class for all readers.
+      class Reader
+        # Yields each word read from given file.
+        # @abstract Subclass and override {#each_word} to fit to a particular
+        #   file format.
+        # @param [String] filepath the full path of the file to load the words
+        #   from.
+        # @yield [String] Each line read from the file.
+        def each_word filepath
+          raise NotImplementedError
+        end
+      end
+    end
+  end
+end
---
+++ lib/rambling/trie/serializers/serializer.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Rambling
+  module Trie
+    module Serializers
+      # Base class for all serializers.
+      class Serializer
+        # Loads contents from a specified filepath.
+        # @abstract Subclass and override {#load} to parse the desired format.
+        # @param [String] filepath the filepath to load contents from.
+        # @return [TContents] parsed contents from given file.
+        def load filepath
+          raise NotImplementedError
+        end
+
+        # Dumps contents into a specified filepath.
+        # @abstract Subclass and override {#dump} to output the desired format.
+        # @param [TContents] contents the contents to dump into given file.
+        # @param [String] filepath the filepath to dump the contents to.
+        # @return [Numeric] number of bytes written to disk.
+        def dump contents, filepath
+          raise NotImplementedError
+        end
+      end
+    end
+  end
+end
