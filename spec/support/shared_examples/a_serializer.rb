7d7166d7e2acf5c7992c297015acb8972768a477
Edgar González <gonzedge@users.noreply.github.com>
2023-05-11 13:50:42 -0400

    Ensure all serializer `#dump` methods return the size of the file (#36)
    - Add test in `a serializer` shared examples
    - Implement method for `Rambling::Trie::Serializers::Zip`

--- spec/support/shared_examples/a_serializer.rb
+++ spec/support/shared_examples/a_serializer.rb
@@ -16,17 +16,22 @@ shared_examples_for 'a serializer' do
   describe '#dump' do
     [true, false].each do |compress_value|
       context "with compressed=#{compress_value} trie" do
-        before do
-          trie.compress! if compress_value
-          serializer.dump content, filepath
+        let(:formatted_content) { format_content.call content }
+
+        before { trie.compress! if compress_value }
+
+        it 'returns the size in bytes of the file dumped' do
+          total_bytes = serializer.dump content, filepath
+          expect(total_bytes).to be_within(20).of formatted_content.size
         end
 
         it 'creates the file with the provided path' do
+          serializer.dump content, filepath
           expect(File.exist? filepath).to be true
         end
 
         it 'converts the contents to the appropriate format' do
-          formatted_content = format_content.call content
+          serializer.dump content, filepath
           expect(File.size filepath).to be_within(20).of formatted_content.size
         end
       end
