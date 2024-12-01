6c914168be868dc71b43762ca53eaa3e7f63f586
Edgar González <edggonzalezg@gmail.com>
2024-12-01 13:31:47 -0500

    ips: compare hash each+assign vs inject
    proves that iterating and copying is faster than `inject` or `each_with_object`
    
    ```
    Warming up --------------------------------------
              var assign   116.860k i/100ms
                  inject    74.373k i/100ms
        each_with_object    76.658k i/100ms
    Calculating -------------------------------------
              var assign      1.167M (± 1.2%) i/s  (856.81 ns/i) -      5.843M in   5.007081s
                  inject    743.505k (± 1.0%) i/s    (1.34 μs/i) -      3.719M in   5.001973s
        each_with_object    763.382k (± 0.9%) i/s    (1.31 μs/i) -      3.833M in   5.021354s
    
    Comparison:
              var assign:  1167124.5 i/s
        each_with_object:   763381.6 i/s - 1.53x  slower
                  inject:   743504.6 i/s - 1.57x  slower
    ```

--- tasks/ips.rb
+++ tasks/ips.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 namespace :ips do
+  task :hash_assign_vs_inject do
+    compare_hash_assign_vs_inject
+  end
+
   task :do_end_vs_brackets do
     compare_do_end_vs_brackets
   end
@@ -52,6 +56,26 @@ def compare
     yield bm
 
     bm.compare!
+  end
+end
+
+def compare_hash_assign_vs_inject
+  compare do |bm|
+    a = { hello: 'there', how: 'do', you: 'do', fellow: 'kids' }
+
+    bm.report 'var assign' do
+      new_hash = {}
+      a.each { |key, value| new_hash[key] = "#{value}-new" }
+      new_hash
+    end
+
+    bm.report 'inject' do
+      a.inject({}) { |new_hash, entry| new_hash[entry[0]] = "#{entry[1]}-new"; new_hash }
+    end
+
+    bm.report 'each_with_object' do
+      a.each_with_object({}) { |entry, new_hash| new_hash[entry[0]] = "#{entry[1]}-new" }
+    end
   end
 end
 
