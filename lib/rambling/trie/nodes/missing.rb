aefdc105af343d80c4957d96c475a26909a69c06
Edgar González <edggonzalezg@gmail.com>
2026-04-19 18:20:12 -0400

    Implement `Nodes::Missing` as a proper null object

--- lib/rambling/trie/nodes/missing.rb
+++ lib/rambling/trie/nodes/missing.rb
@@ -5,6 +5,23 @@ module Rambling
     module Nodes
       # A representation of a missing node in the trie data structure. Returned when a node is not found.
       class Missing < Rambling::Trie::Nodes::Node
+        def partial_word? _chars
+          false
+        end
+
+        private
+
+        def word_chars? _chars
+          false
+        end
+
+        def closest_node _chars
+          self
+        end
+
+        def children_match_prefix _chars
+          return enum_for :children_match_prefix, _chars unless block_given?
+        end
       end
     end
   end
