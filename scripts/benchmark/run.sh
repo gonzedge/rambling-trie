cf2ce852d398b47aee8b7ec44d94206fa02eff4b
Edgar González <edggonzalezg@gmail.com>
2026-04-25 15:10:55 -0400

    Allow benchmark/run.sh to skip cache and use verbose docker output with new args

--- scripts/benchmark/run.sh
+++ scripts/benchmark/run.sh
@@ -3,10 +3,12 @@
 set -eo pipefail
 
 from_github=false
+no_cache=false
 ruby_version=3.3.6
+verbose=false
 version=latest
 
-OPT_STRING="gv:"
+OPT_STRING="gnv:V"
 
 while getopts ${OPT_STRING} opt; do
   case ${opt} in
@@ -22,8 +24,18 @@ while getopts ${OPT_STRING} opt; do
     g) # use git ref instead of rubygems version
       from_github=true
       echo "from_github set to ${from_github}";;
+    n) # skip docker layer cache
+      no_cache=true
+      echo "no_cache set to ${no_cache}";;
+    V) # verbose docker build output
+      verbose=true
+      echo "verbose set to ${verbose}";;
   esac
 done
+
+docker_build_args=()
+[[ "${no_cache}" == "true" ]] && docker_build_args+=(--no-cache)
+[[ "${verbose}" == "true" ]] && docker_build_args+=(--progress=plain)
 
 echo "Building benchmark image for version=${version}, ruby=${ruby_version}, from_github=${from_github}"
 
@@ -32,7 +44,8 @@ docker build . --file Dockerfile.benchmark \
   --build-arg VERSION=${version} \
   --build-arg FROM_GITHUB=${from_github} \
   --build-arg RUBY_VERSION=${ruby_version} \
-  --tag ${image} && \
+  --tag ${image} \
+  "${docker_build_args[@]}" && \
   echo "Image ${image} built!"
 
 output_file="tmp/${version}@${ruby_version}.benchmark"
